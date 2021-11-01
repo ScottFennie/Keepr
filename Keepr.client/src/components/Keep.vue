e30fd6b5e0fd5eceea18db136d591c16d63b40ff
Scott Fennie <s.fennie7@gmail.com>
2021-10-31 21:42:47 -0600

    Started on the profile card

---
+++ Keepr.client/src/components/Keep.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="col-3 card">
+  <div class="d-flex justify-content-between">
+    <h3>{{keep.title}}</h3>
+    <img class="rounded-circle" src="creator.picture" alt="profile image">
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
