f10bd015d750f93d2c1772e341b78bae69c182f5
Scott Fennie <s.fennie7@gmail.com>
2021-11-02 09:55:34 -0600

    started building out profile page

---
+++ Keepr.client/src/services/ProfileService.js
@@ -0,0 +1,12 @@
+import { AppState } from "../AppState"
+import { Keep } from "../models/Keep"
+import { logger } from "../utils/Logger"
+import { api } from "./AxiosService"
+
+class ProfileService{
+
+    async getProfileById(getProfileById){
+
+    }
+}
+export const profileService = new ProfileService()
