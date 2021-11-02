fa40668f9baff88919444630ed3fc2ca18e857fb
Scott Fennie <s.fennie7@gmail.com>
2021-11-02 09:39:48 -0600

    fixed home page

--- Keepr.client/src/pages/HomePage.vue
+++ Keepr.client/src/pages/HomePage.vue
@@ -1,12 +1,7 @@
 <template>
-<div class="container-fluid">
-  <div class="row">
+  <div class="masonry">
     <Keep :keep="k" v-for="k in keeps" :key="k.id" />
-    <div class="col-12">
-      <h1>Test</h1>
-    </div>
   </div>
-</div>
 </template>
 
 <script>
@@ -29,6 +24,7 @@ export default {
    }
  }
 }
+
 </script>
 
 <style scoped lang="scss">
@@ -49,4 +45,46 @@ export default {
     }
   }
 }
+
+/* The Masonry Container */
+.masonry {
+  margin: 1.5em auto;
+  max-width: 768px;
+  column-gap: 1.5em;
+  flex-wrap: wrap;
+  flex-flow: column wrap;
+}
+
+/* The Masonry Brick */
+.item {
+  background: #fff;
+  padding: 1em;
+  margin: 0 0 1.5em;
+}
+
+/* Masonry on large screens */
+@media only screen and (min-width: 1024px) {
+  .masonry {
+    column-count: 3;
+  }
+}
+
+/* Masonry on medium-sized screens */
+@media only screen and (max-width: 1023px) and (min-width: 768px) {
+  .masonry {
+    column-count: 3;
+  }
+}
+
+/* Masonry on small screens */
+@media only screen and (max-width: 767px) and (min-width: 540px) {
+  .masonry {
+    column-count: 2;
+  }
+}
+
+
+
+
+
 </style>
