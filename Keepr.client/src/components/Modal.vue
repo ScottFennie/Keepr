26c1259f92593f2662fa50574794d3eb6218be50
Scott Fennie <s.fennie7@gmail.com>
2021-11-03 11:28:18 -0600

    changed opacity of modals

--- Keepr.client/src/components/Modal.vue
+++ Keepr.client/src/components/Modal.vue
@@ -30,7 +30,7 @@ export default {
     margin: 0;
   box-shadow: 0 0 1rem 0 rgba(0, 0, 0, .2); 
   border-radius: 5px;
-  background-color: rgba(15, 4, 4, 0.15);
+  background-color: rgba(255, 255, 255, 0.15);
   
   backdrop-filter: blur(10px);
 }
